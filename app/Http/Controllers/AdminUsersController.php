b30e1bcf16e8497e2a730751a17f0d56c70e7746
ASarvar <sarvar.bakhtiyor@gmail.com>
2018-09-06 23:07:40 +0500

    User created correctly

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Http\Requests\UsersRequest;
+use App\User;
+use App\Role;
 use App\Http\Requests;
 
 class AdminUsersController extends Controller
@@ -15,7 +17,10 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        //
+
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -25,7 +30,10 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        //
+
+        $roles = Role::lists('name', 'id')->all();
+
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -34,9 +42,13 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
-        //
+        User::create($request->all());
+
+        return redirect('/admin/users');
+
+//        return $request->all();
     }
 
     /**
@@ -47,7 +59,7 @@ class AdminUsersController extends Controller
      */
     public function show($id)
     {
-        //
+//        return view('admin.users.create');
     }
 
     /**
@@ -58,7 +70,7 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        //
+//        return view('admin.users.create');
     }
 
     /**
@@ -70,7 +82,7 @@ class AdminUsersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+//        return view('admin.users.create');
     }
 
     /**
@@ -81,6 +93,6 @@ class AdminUsersController extends Controller
      */
     public function destroy($id)
     {
-        //
+//        return view('admin.users.create');
     }
 }
